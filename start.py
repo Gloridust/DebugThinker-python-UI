0ac9134a75747e3d159634f287443c30e97a6c9e
Gloridust <Gloridust@gmail.com>
2023-11-13 15:44:20 +0800

    Update start.py

--- start.py
+++ start.py
@@ -32,15 +32,14 @@ class ChatApp:
         self.run_button.pack(pady=10)
 
     def run_program(self):
-        
-        self.print_to_output("正在处理中，请稍后...")
-
         context_preset = "假设你是一个有丰富经验的软件开发工程师。我可能会提供一些关于软件开发的具体问题，这些问题信息可能是需要您修改的有Bug无法运行的程序，也有可能是终端中的报错代码，还有可能是其他相关内容。您的工作是简明扼要地站在初学者的角度，分析程序故障原因，作出修改，并指出错在哪里和为什么这样修改，这可能包括建议代码、代码逻辑思路策略。请直接针对下面输入的报错代码与补充描述内容进行回答，无需多说其他内容。"
         context_code = "以下是报错的代码：" + self.context_code_entry.get("1.0", tk.END)
         context_word = "以下是补充描述内容：" + self.context_word_entry.get("1.0", tk.END)
         context = context_preset + context_code + context_word
 
         self.output_text.delete(1.0, tk.END)  # 清空输出框内容
+
+        self.print_to_output("正在处理中，请稍后...")
 
         url = "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/completions?access_token=" + self.get_access_token()
 
